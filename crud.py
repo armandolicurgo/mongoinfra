32a615bc53c785f20f59cb91ad8e6395e8f5ad72
armandolicurgo <armandolicurgo@gmail.com>
2018-06-14 01:42:03 -0300

    Update crud.py

--- crud.py
+++ crud.py
@@ -3,7 +3,29 @@
 # crud.py -init database
 # crud.py -init database colection1 colection2 colection3
 # crud.py 
+import pymongo
+import json
+
 
 class Crud():
-  pass
-
+	um crud para cada collection
+	cliente = `` # nao inicializado
+	
+	def init(url=``,collect=``):
+		cliente = MongoClient(url)
+		return cliente
+	def c(jso):
+		pass # return _id
+	def r(id):
+		pass # return jso
+	def u(id):
+		pass # return jso
+	def d(id):
+		pass # return js
+	def open(url):
+		pass  # return database
+	def sync(dic,jso):
+  	pass
+	def dodefault():
+		crd = Crud('mongodb://localhost:27017/','test')
+  	
