2660b55c76b5689043d58c5a4cb085ebb2a8af25
armandolicurgo <armandolicurgo@gmail.com>
2018-06-15 18:04:42 -0300

    Update crud.py

--- crud.py
+++ crud.py
@@ -5,6 +5,9 @@
 # crud.py 
 import pymongo
 import json
+import string
+#from pymongo import MongoClient
+import matplotlib.pyplot as plt
 
 
 class Crud():
@@ -30,3 +33,60 @@ class Crud():
 	def dodefault():
 		crd = Crud('mongodb://localhost:27017/','test')
   	
+        def Consulta1(self):
+                #import string
+                #from pymongo import MongoClient
+
+                ff= open(  "consulta1.txt","w")
+
+                client = MongoClient('localhost', 27017)
+                query = {"ver": {"$gt" : 6}}
+                campos = {"coletado": 1,"url": 1,"inicio": 1, "fim": 1}
+                ff.close()
+                database = client["test"]
+                collection = database["coletados"]
+
+                # Created with Studio 3T, the IDE for MongoDB - https://studio3t.com/
+
+                maxi = 10000
+                i=0
+                cursor = collection.find(query, campos)
+                try:
+                        for doc in cursor:
+                                i+=1
+                                if (i < maxi):
+                                        print( doc )
+                finally:
+                    client.close()
+
+		
+        def Consulta2Grafico(self):
+                #import string
+                #from pymongo import MongoClient
+                #import matplotlib.pyplot as plt
+                ff= open(  "consulta1.txt","w")
+                cliente = MongoClient('localhost', 27017)
+                banco = cliente.test
+
+                coletado = {"ver" :  8 }
+                campos = {"url": 1,"inicio": 1, "fim": 1}
+                sr = {"url": 1,"inicio": 1}
+
+                alb = banco.coletados
+                m_id = alb.find(coletado, campos)
+                print( m_id )
+                x,y = [],[]
+                for kk in m_id:
+                        i =int(kk['inicio'])
+                        f =int(kk['fim'])
+                        print( i , f)
+                        if (0 < f < 1000):
+                                x.append(i)
+                                y.append(f)
+
+                ff.close()
+
+                plt.style.use('seaborn-whitegrid')
+                fig,ax = plt.subplots()
+                ax.plot(x, y,"bo")
+                plt.show()
