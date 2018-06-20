6f99ce7dd7526c23a3b98f6ec1f24b9ee438c7da
Armando Licurgo da Silva <armandolicurgo@gmail.com>
2018-06-20 13:48:44 -0300

    Update recursos.py

--- recursos.py
+++ recursos.py
@@ -9,3 +9,20 @@
 
 
 """
+{
+    "id": "<item_id>", 
+    "summary": "<one-line summary>"
+}
+{
+    "id": "<item_id>", 
+    "summary": "<one-line summary>", 
+    "description" : "<free-form text field>"
+}
+import requests
+
+resp = requests.get('https://todolist.example.com/tasks/')
+if resp.status_code != 200:
+    # This means something went wrong.
+    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+for todo_item in resp.json():
+    print('{} {}'.format(todo_item['id'], todo_item['summary']))
