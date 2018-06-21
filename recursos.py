e09fc635aead318fe58fe8ea470b1a95c4eae80d
Armando Licurgo da Silva <armandolicurgo@gmail.com>
2018-06-21 05:00:05 -0300

    Update recursos.py

--- recursos.py
+++ recursos.py
@@ -6,17 +6,22 @@ interf = """/test/coletados/_id/
 /test/jobs/"""
 inter = inter.split("\n")
 
-def retornar(p):
+def return_(p):
 	print(p)
-
+def exec_(rq_find,rq)
+	print(rq_find,rq)
+	# usar mongo
+	
 def trataRequest(rq):
 	i=0	
 	for one in interf:
-		rq_find = rq.find(cada)
+		rq_find = rq.find(one)
 		if (rq_find != -1):
+			return_(exec_(rq_find,rq))
 			break
 		i+=1
 	if (i>len(inter)):
-		retornar("501")
+		return_("501")
 
 trataRequest("http://test/jobs/")
+trataRequest("http://test/clientes/")
