6c6f6318faa402d9d38617221fb34e8f5dae8889
Armando Licurgo da Silva <armandolicurgo@gmail.com>
2018-06-21 04:52:14 -0300

    Update recursos.py

--- recursos.py
+++ recursos.py
@@ -1,11 +1,22 @@
 #implementar:
 
-"""
-/coletados/id 
-/coletados/
-/jobs/id/sites/
-/jobs/id/
-/jobs/
+interf = """/test/coletados/_id/ 
+/test/coletados/
+/test/jobs/_id/
+/test/jobs/"""
+inter = inter.split("\n")
 
+def retornar(p):
+	print(p)
 
-"""
+def trataRequest(rq):
+	i=0	
+	for one in interf:
+		rq_find = rq.find(cada)
+		if (rq_find != -1):
+			break
+		i+=1
+	if (i>len(inter)):
+		retornar("501")
+
+trataRequest("http://test/jobs/")
