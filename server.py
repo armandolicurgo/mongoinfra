8a49d49af795dc9fb7344c2e3fde78e57cff65e5
Armando Licurgo da Silva <armandolicurgo@gmail.com>
2018-06-23 19:12:42 -0300

    Update server.py

--- server.py
+++ server.py
@@ -2,6 +2,8 @@
  
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+import crud
+
 
 
 # HTTPRequestHandler class
@@ -16,8 +18,8 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type','text/html')
         self.end_headers()
  
-        # Send message back to client
-        message = "Hello world!"
+       	k = crud.Crud()
+        message = k.trataRequest("http://test/jobs/")
         # Write content as utf-8 data
         self.wfile.write(bytes(message, "utf8"))
         return
