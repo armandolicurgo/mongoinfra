fcf46c7fe2ad773b05f9a501ec3f9e532920cc1e
Armando Licurgo da Silva <armandolicurgo@gmail.com>
2018-08-15 21:00:32 -0300

    Update crud.py

--- crud.py
+++ crud.py
@@ -2,36 +2,3 @@
 import json
 import string
 from pymongo import MongoClient
-
-interf = """/test/coletados/_id/ 
-/test/coletados/
-/test/jobs/_id/
-/test/jobs/"""
-interf = interf.split("\n")
-
-
-
-class Crud():
-	cliente = ''
-	def __init__(self,u='mongodb://localhost:27017/',c='test'):
-		cliente = MongoClient(u)
-	def dodefault(self):
-		crd = Crud('mongodb://localhost:27017/','test')	
-	def Consulta1(self):
-		ff= open(  "consulta1.txt","w")
-		client = MongoClient('localhost', 27017)
-		query = {"ver": {"$gt" : 6}}
-		campos = {"coletado": 1,"url": 1,"inicio": 1, "fim": 1}
-		ff.close()
-		database = client["test"]
-		collection = database["coletados"]
-		maxi = 10000
-		i=0
-		cursor = collection.find(query, campos)
-		try:
-			for doc in cursor:
-				i+=1
-				if (i < maxi):
-					print( doc )
-		finally:
-			client.close()
