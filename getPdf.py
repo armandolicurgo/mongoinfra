0b18e7f4119619e9435106b23be5aef44d700751
Armando Licurgo da Silva <armandolicurgo@gmail.com>
2018-08-15 21:52:24 -0300

    Add files via upload

---
+++ getPdf.py
@@ -0,0 +1,25 @@
+import PyPDF2
+pdfFileObj = open('constituicao_ingles_3ed.pdf', 'rb')
+
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+allpages = (pdfReader.numPages)
+
+alltext = ""
+x = open('constituicao_ingles_3ed.pdf.txt',"w")
+
+for page in range(allpages):
+	pageObj = pdfReader.getPage(page)
+	thispage = pageObj.extractText()
+	print(thispage)
+	try:
+		x.write(thispage)
+	except:
+		x.write("EEERRROOORRR")	
+	alltext += thispage
+
+
+x.close()
+
+
+
